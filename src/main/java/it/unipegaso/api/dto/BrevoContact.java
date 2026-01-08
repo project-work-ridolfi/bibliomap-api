8be8775a501a995630dc0e0bf93bdf2aa58dc973
Ad <93925469+AdrianaRidolfi@users.noreply.github.com>
2026-01-08 09:37:12 +0100

    Add BrevoContact DTO with name and email fields

---
+++ src/main/java/it/unipegaso/api/dto/BrevoContact.java
@@ -0,0 +1,8 @@
+package it.unipegaso.api.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+public record BrevoContact(
+    @JsonProperty("name") String name,
+    @JsonProperty("email") String email
+) {}
