5785f44c9b89334cbc8cf16f0d4519566c563e3e
Ad <93925469+AdrianaRidolfi@users.noreply.github.com>
2026-01-08 09:36:47 +0100

    Create BrevoRequest record for API DTO

---
+++ src/main/java/it/unipegaso/api/dto/BrevoRequest.java
@@ -0,0 +1,11 @@
+package it.unipegaso.api.dto
+
+import java.util.List
+import com.fasterxml.jackson.annotation.JsonProperty
+
+public record BrevoRequest(
+    @JsonProperty("sender") BrevoContact sender,
+    @JsonProperty("to") List<BrevoContact> to,
+    @JsonProperty("subject") String subject,
+    @JsonProperty("htmlContent") String htmlContent
+) {}
