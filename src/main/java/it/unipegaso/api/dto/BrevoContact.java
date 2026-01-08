8f6d585bf0e863bb5452bc05bc8a3ac77af96a32
Ad <93925469+AdrianaRidolfi@users.noreply.github.com>
2026-01-08 11:42:59 +0100

    Add JsonInclude annotation to BrevoContact

--- src/main/java/it/unipegaso/api/dto/BrevoContact.java
+++ src/main/java/it/unipegaso/api/dto/BrevoContact.java
@@ -1,7 +1,9 @@
 package it.unipegaso.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record BrevoContact(
     @JsonProperty("name") String name,
     @JsonProperty("email") String email
