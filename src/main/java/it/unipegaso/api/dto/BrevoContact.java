2c9360f3f7761ad814a29a80ad972bf1bb044cf2
Ad <93925469+AdrianaRidolfi@users.noreply.github.com>
2026-01-08 09:58:38 +0100

    Fix import statement for JsonProperty annotation

--- src/main/java/it/unipegaso/api/dto/BrevoContact.java
+++ src/main/java/it/unipegaso/api/dto/BrevoContact.java
@@ -1,6 +1,6 @@
 package it.unipegaso.api.dto
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BrevoContact(
     @JsonProperty("name") String name,
