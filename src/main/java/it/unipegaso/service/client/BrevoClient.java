40f4c017030a7cbbef2d0bcc6a004f639ee8f54d
Ad <93925469+AdrianaRidolfi@users.noreply.github.com>
2026-01-08 09:30:21 +0100

    Add BrevoClient interface for email sending

---
+++ src/main/java/it/unipegaso/service/client/BrevoClient.java
@@ -0,0 +1,17 @@
+package it.unipegaso.service.client;
+
+import it.unipegaso.database.dto.BrevoRequest;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(configKey = "brevo-api")
+public interface BrevoClient {
+    @POST
+    @Path("/v3/smtp/email")
+    void sendEmail(
+        @HeaderParam("api-key") String apiKey,
+        BrevoRequest request
+    ) throws Exception;
+}
